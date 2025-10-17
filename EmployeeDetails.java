49aae81a515181f3b9402eb123e6478d469e900b
jananya98 <jananyaeee@gmail.com>
2025-10-17 18:49:35 +0530

    Day1 assignments

---
+++ EmployeeDetails.java
@@ -0,0 +1,38 @@
+package week1.day1;
+
+public class EmployeeDetails {
+public static void main(String[] args) {
+	String name = "John Doe";
+	byte  age = 30;
+	double salary = 55000.75;
+	char gender = 'M';
+	boolean acceptTerms = true;
+	 float rating = 4.5f;
+	 long mobileNumber = 9876543210l;
+	int pinCode = 56001;
+	byte luckyNumber = 7;
+	System.out.println("Personal Information of the Employee:\n" +
+            "Employee Name: " + name + "\n" +
+            "Age: " + age + "\n" +
+            "Gender: " + gender + "\n" +
+            "Salary: $" + salary + "\n" +
+            "Additional Information of the Employee:\n"+
+            "Accept Terms: " + acceptTerms + "\n" +
+            "Rating: " + rating + "\n" +
+            "Mobile Number: " + mobileNumber + "\n" +
+            "Pincode: " + pinCode + "\n" +
+            "Lucky Number: " + luckyNumber);
+
+
+	 
+
+
+
+
+	
+
+
+	
+
+}
+}
